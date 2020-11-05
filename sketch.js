ed2783d71e64b4822f6455d5201a85448d78a766
ridhima1211 <ridhima.agarwal.2008@gmail.com>
2020-11-05 19:42:58 +0530

    Diwali

--- sketch.js
+++ sketch.js
@@ -7,10 +7,12 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var gameState = "onSling";
+var bg = "sprites/bg.png";
+var score = 0;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImg();
 }
 
 function setup(){
@@ -44,18 +46,27 @@ function setup(){
 }
 
 function draw(){
+    if (backgroundImg)
     background(backgroundImg);
+
+noStroke();
+textSize(35);
+fill ("white");
+text("Score : "+score, width-300, 50);
+
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -69,16 +80,40 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gameState !=="launched" ){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
-
+}
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched";
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+   //     slingshot.attach(bird.body);
     }
 }
+
+async function getBackgroundImg(){
+var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+var responseJSON = await response.json();
+
+var datetime = responseJSON.datetime;
+var hour = datetime.slice(11,13);
+
+if(hour>=06 && hour<=19){
+bg = "sprites/bg.png";
+
+}
+else{
+bg = "sprites/bg2.jpg";
+
+}
+backgroundImg = loadImage(bg);
+
+}
+
+
+
